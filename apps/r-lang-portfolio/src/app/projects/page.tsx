b944bbdfd2535d5b4bbaf9ef015e42e552b03b69
Rebecca Lang <rebeccalang50@gmail.com>
2025-03-06 14:59:08 +1300

    updated projects page with info from github api

--- apps/r-lang-portfolio/src/app/projects/page.tsx
+++ apps/r-lang-portfolio/src/app/projects/page.tsx
@@ -1,8 +1,30 @@
-export default function Projects() {
+async function getRepos() {
+  const res = await fetch('https://api.github.com/users/Rebecca-Llang/repos', {
+    next: { revalidate: 172800 },
+  });
+  const data = await res.json();
+  const filteredRepos = data.filter((repo) => repo.private === false);
+  return filteredRepos;
+}
+
+export default async function Projects() {
+  const repos = await getRepos();
+
   return (
     <div>
       <h1>Projects</h1>
-      {/* Your about page content */}
+      <h2>My Github Projects</h2>
+
+      <ul>
+        {repos.map((repo) => (
+          <li key={repo.id}>
+            <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
+              {repo.name}
+            </a>
+            <p>{repo.description || 'No description available.'}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
