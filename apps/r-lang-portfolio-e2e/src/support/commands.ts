7f9119d5120b96873e5527ee06333d2b27aae0ec
Rebecca Lang <rebeccalang50@gmail.com>
2025-04-23 11:16:04 +1200

    checkpoint before force audit fix

--- apps/r-lang-portfolio-e2e/src/support/commands.ts
+++ apps/r-lang-portfolio-e2e/src/support/commands.ts
@@ -1,21 +1,20 @@
-/// <reference types="cypress" />
-declare global {
-  namespace Cypress {
-    interface Chainable {
-      contact(name: string, email: string, message: string): Chainable;
-    }
-  }
-}
+// /// <reference types="cypress" />
 
-Cypress.Commands.add(
-  'contact',
-  (name: string, email: string, message: string) => {
-    cy.visit('/contact-me');
-    cy.get('input[name="name"]').type(name);
-    cy.get('input[name="email"]').type(email);
-    cy.get('textarea[name="message"]').type(message);
-    cy.get('form').submit();
-  }
-);
+// export {};
 
-export {};
+// declare global {
+//   interface Chainable {
+//     contact(name: string, email: string, message: string): Chainable<void>;
+//   }
+// }
+
+// Cypress.Commands.add(
+//   'contact',
+//   (name: string, email: string, message: string) => {
+//     cy.visit('/contact-me');
+//     cy.get('input[name="name"]').type(name);
+//     cy.get('input[name="email"]').type(email);
+//     cy.get('textarea[name="message"]').type(message);
+//     cy.get('form').submit();
+//   }
+// );
